d0d208797ad5b4a6d47508df26dee1e869ba3f04
Philipp <36186933+PhilippSaminger@users.noreply.github.com>
2023-11-10 10:51:28 +0100

    Add files via upload
    Add xcode cloud repo

---
+++ xcodeCloud/xcodeCloud/xcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeCloudApp.swift
+//  xcodeCloud
+//
+//  Created by Philipp Saminger on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct xcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4737a95cd6978aa6cb0eea38aff5dc5df8232471
PhilippSaminger <Philipp.Saminger@netcetera.com>
2023-11-13 10:37:50 +0100

    Add dependencies

--- xcodeCloud/xcodeCloudTests/xcodeCloudTests.swift
+++ xcodeCloud/xcodeCloudTests/xcodeCloudTests.swift
@@ -6,31 +6,42 @@
 //
 
 import XCTest
-@testable import xcodeCloud
+@testable import NCAXcodeCloudDemo
 
-final class xcodeCloudTests: XCTestCase {
+class CalculatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var calculator: Calculator!
+
+    override func setUp() {
+        super.setUp()
+        calculator = Calculator()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        calculator = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAddition() {
+        XCTAssertEqual(calculator.add(2, 3), 5, "Addition is incorrect")
+        XCTAssertEqual(calculator.add(-2, 3), 1, "Addition with negative numbers is incorrect")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSubtraction() {
+        XCTAssertEqual(calculator.subtract(5, 3), 2, "Subtraction is incorrect")
+        XCTAssertEqual(calculator.subtract(3, 5), -2, "Subtraction with negative result is incorrect")
     }
 
+    func testMultiplication() {
+        XCTAssertEqual(calculator.multiply(2, 3), 6, "Multiplication is incorrect")
+        XCTAssertEqual(calculator.multiply(0, 5), 0, "Multiplication by zero is incorrect")
+    }
+
+    func testDivision() {
+        XCTAssertEqual(calculator.divide(6, 3), 2, "Division is incorrect")
+        XCTAssertEqual(calculator.divide(5, 2), 2, "Division with remainder is incorrect")
+        
+        // Test division by zero
+        XCTAssertNil(calculator.divide(4, 0), "Division by zero should return nil")
+    }
 }
